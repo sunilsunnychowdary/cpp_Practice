7be84fcbe2e0476f93240061b8c2e32f775b719e
sunilsunnychowdary <83498229+sunilsunnychowdary@users.noreply.github.com>
2021-09-21 22:40:00 +0530

    Add files via upload

---
+++ tut1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+
+using namespace std;
+
+int main()
+{
+    int n1, n2;
+    cout<<"Enter number n1:\n";
+    cin>> n1;
+    cout<<"Enter number n2:\n";
+    cin>> n2;   
+    cout<<"sum:"<<n1+n2;
+    return 0;
+
+}
---
+++ tut2.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    program"< cout<<"this is hello world<endl;
+    // int a=4,b=5;
+    //here i am doing some operations
+    
+    // cout<<"a+b value="<<a+b<<endl;
+    // cout<<"a-b value="<<a-b<<endl;
+    // cout<<"a*b value="<<a*b<<endl;
+    // cout<<"a/b value="<<a/b<<endl;
+    // float x= 455;
+    // cout<<x<<endl;
+    // float & y = x;
+    // cout<<y<<endl;
+    string str;
+    cout<<"What is your name?";
+    getline(cin,str);
+    cout<<"Hello, "<<str;
+
+    
+    return 0;
+
+}
